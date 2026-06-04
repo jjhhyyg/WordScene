1180b58803f1a7918e138008971d813d6db79213
erikssonhou <erikssonhou@gmail.com>
2026-06-04 17:31:57 +0800

    Add share extension target

---
+++ WordScene/Sources/ShareExtension/ShareViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
